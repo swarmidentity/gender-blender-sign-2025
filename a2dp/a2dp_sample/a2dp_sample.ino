3c260ba0bebd24e36cbe963949191ca44a3eda83
Louise Racette <louise@taroworks.org>
2025-06-03 16:07:20 -0700

    A2dp test (too large to fit on ESP32 DEVKIT v1)

---
+++ a2dp/a2dp_sample/a2dp_sample.ino
@@ -0,0 +1,31 @@
+#include "AudioTools.h"
+#include "BluetoothA2DPSink.h"
+
+AnalogAudioStream out;
+BluetoothA2DPSink a2dp_sink(out);
+
+void setup() {
+    a2dp_sink.set_stream_reader(read_data_stream);
+    a2dp_sink.set_avrc_metadata_callback(avrc_metadata_callback);
+    a2dp_sink.set_avrc_rn_volumechange(avrc_volume_change);
+    a2dp_sink.start("GenderBlender");
+}
+
+void loop() {
+}
+
+// Then somewhere in your sketch:
+void read_data_stream(const uint8_t *data, uint32_t length)
+{
+  int16_t *samples = (int16_t*) data;
+  uint32_t sample_count = length/2;
+  // Do something with the data packet
+}
+
+void avrc_metadata_callback(uint8_t data1, const uint8_t *data2) {
+  Serial.printf("AVRC metadata rsp: attribute id 0x%x, %s\n", data1, data2);
+}
+
+void avrc_volume_change(int data1) {
+  //Serial.printf("AVRC Volume rsp: attribute id 0x%x\n", data1);
+}
