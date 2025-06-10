a428fcff46ad4b6bc3b6445952a47154fbb58d35
Louise Racette <louise@taroworks.org>
2025-06-09 18:01:55 -0700

    Working rainbow and individual letter placeholders

--- bluetooth_swapped_light_patterns/bluetooth_swapped_light_patterns.ino
+++ bluetooth_swapped_light_patterns/bluetooth_swapped_light_patterns.ino
@@ -28,8 +28,52 @@ enum LEDPattern {
     RAINBOW = 0,
     TRANS_FLAG_STATIC = 1,
     MIXED_RAINBOW_TRANS_FLAG = 2,
+    SEPARATE_LETTER_COLORS = 3,
     // Add more patterns as needed
 };
+
+const int LETTER_COUNT = 13;
+
+// Start and end pixel indices for each letter in "GENDERBLENDER"
+// Example: {start, end} for each letter (inclusive start, exclusive end)
+const int LETTER_PIXEL_RANGES[LETTER_COUNT][2] = {
+    {0, 20},    // G
+    {22, 42},   // E
+    {44, 64},   // N
+    {66, 86},   // D
+    {88, 108},  // E
+    {110, 130}, // R
+    {132, 152}, // B
+    {154, 174}, // L
+    {176, 196}, // E
+    {198, 218}, // N
+    {220, 240}, // D
+    {242, 264}, // E
+    {266, 300}  // R
+};
+
+// Optional: Character labels for reference
+const char* LETTER_LABELS[LETTER_COUNT] = {
+    "G", "E", "N", "D", "E", "R", "B", "L", "E", "N", "D", "E", "R"
+};
+
+// Example colors for each letter (customize as needed)
+const uint32_t LETTER_COLORS[LETTER_COUNT] = {
+    pixels.Color(255, 0, 0),    // Red
+    pixels.Color(255, 128, 0),  // Orange
+    pixels.Color(255, 255, 0),  // Yellow
+    pixels.Color(0, 255, 0),    // Green
+    pixels.Color(0, 255, 255),  // Cyan
+    pixels.Color(0, 0, 255),    // Blue
+    pixels.Color(128, 0, 255),  // Purple
+    pixels.Color(255, 0, 255),  // Magenta
+    pixels.Color(255, 0, 128),  // Pink
+    pixels.Color(128, 128, 128),// Gray
+    pixels.Color(255, 255, 255),// White
+    pixels.Color(128, 64, 0),   // Brown
+    pixels.Color(50, 50, 50)       // Grey
+};
+
 
 LEDPattern currentPattern = RAINBOW;
 
@@ -83,21 +127,20 @@ void switchBetweenLEDControlPatterns() {
     else if (currentPattern == MIXED_RAINBOW_TRANS_FLAG) {
         mixedRainbowTransFlagPattern();
     }   
+    else if (currentPattern == SEPARATE_LETTER_COLORS) {
+        setEachLetterToDifferentColor();
+    }
 }
 
 void fullRainbowPattern() {
-    static uint8_t rainbowHue = 0;
+    static uint16_t rainbowHue = 0;
     rainbowHue++; // Animate the rainbow
     if (rainbowHue == NUMPIXELS) {
       rainbowHue = 0;
     }
-
-    for (int i = 0; i < NUMPIXELS; i++) {
-      pixels.setPixelColor(i,pixels.ColorHSV((rainbowHue + i )  * ( 65536/NUMPIXELS) ));
-    }
-
+    drawRainbowSection(0, NUMPIXELS, rainbowHue); 
     pixels.show();
-    delay(DELAYVAL);
+    delay(DELAYVAL/3);
 }
 
 void staticTransFlagPattern() {
@@ -124,8 +167,15 @@ void drawTransFlag(int startPos, int endPos) {
     }
 }
 
+void drawRainbowSection(int startPos, int endPos, uint16_t rainbowHue) {
+    int sectionLength = endPos - startPos;
+    for (int i = startPos; i < endPos; i++) {
+        pixels.setPixelColor(i, pixels.ColorHSV((int)((rainbowHue + i - sectionLength/2) * (65536.0 / sectionLength))));
+    }
+}
+
 void mixedRainbowTransFlagPattern() {
-    static uint8_t rainbowHue = 0;
+    static uint16_t rainbowHue = 0;
     rainbowHue++; // Animate the rainbow
 
     int sectionLength = NUMPIXELS / 3; 
@@ -134,20 +184,32 @@ void mixedRainbowTransFlagPattern() {
       rainbowHue = 0;
     }
     
-
     // Rainbow on 0-99
-    for (int i = 0; i < sectionLength; i++) {
-      pixels.setPixelColor(i,pixels.ColorHSV((rainbowHue + i )  * ( 65536/sectionLength) ));
-    }
+    drawRainbowSection(0, sectionLength, rainbowHue); 
 
     // Trans flag on 100-199
     drawTransFlag(sectionLength, sectionLength * 2);
 
     // Rainbow on 200-299
-    for (int i = sectionLength * 2; i < sectionLength * 3; i++) {
-      pixels.setPixelColor(i,pixels.ColorHSV((rainbowHue + (i-(2* sectionLength)) )  * ( 65536/sectionLength) ));
-    }
+    drawRainbowSection(sectionLength * 2, sectionLength * 3, rainbowHue);
 
     pixels.show();
     delay(DELAYVAL);
+}
+
+// Sample function to set each letter to a different color
+void setEachLetterToDifferentColor() {
+    // Clear all pixels first
+    for (int i = 0; i < NUMPIXELS; ++i) {
+        pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // Set all pixels to black
+    }
+    for (int i = 0; i < LETTER_COUNT; ++i) {
+        int start = LETTER_PIXEL_RANGES[i][0];
+        int end = LETTER_PIXEL_RANGES[i][1];
+        uint32_t color = LETTER_COLORS[i];
+        for (int j = start; j < end; ++j) {
+            pixels.setPixelColor(j, color);
+        }
+    }
+    pixels.show();
 }
